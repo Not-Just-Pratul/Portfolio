da1b3ac548e1f76108f7b66ae7c4e3599b2d53b9
Pratul Sharma <146079557+Not-Just-Pratul@users.noreply.github.com>
2025-11-07 13:53:26 +0530

    Initial clean commit

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -77,7 +77,7 @@ const projects = [
   },
     {
     id: 9,
-    title: 'AI Prompt Enhancer,
+    title: 'AI Prompt Enhancer',
     description: 'The simplest way to create better prompts with AI',
     tags: ['React', 'TypeScript', 'Vite', 'Shad-cn', 'Gen AI'],
     image: 'https://plus.unsplash.com/premium_photo-1725907643556-e987fab4b09d??w=800&h=600&fit=crop',
